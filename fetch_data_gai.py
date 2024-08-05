52211bcd42c148e22457048631796b070c397b7e
XuYue <AstraWhale@gmail.com>
2024-08-06 02:29:41 +0800

    better logger, gai2 is useless so deleted

--- fetch_data_gai.py
+++ fetch_data_gai.py
@@ -4,56 +4,69 @@ import json
 import os
 import time
 
+
 class MyLogger:
-    def __init__(self,rate = 10):
+    def __init__(self, rate=10):
         self.rate = rate
         self.counter = 1
         self.last_msg_type = None
 
-    def log(self,type,msg):
-        self.counter+=1
+    def log(self, type, msg):
+        self.counter += 1
         if self.counter % self.rate == 0 or self.last_msg_type != type:
             print(msg)
+            with open("./log/log.txt", 'a') as file:
+                file.write(msg)
             self.counter = 1
             self.last_msg_type = type
 
+
 my_logger = MyLogger(50)
+
 
 async def get_wss_data(url):
     os.makedirs("./data", exist_ok=True)  # Ensure the data directory exists
-    
+
     async with websockets.connect(url) as websocket:
         while True:
-            formatted_time = time.strftime("%Y-%m-%d-%H-%M-%S", time.localtime())
+            formatted_time = time.strftime(
+                "%Y-%m-%d-%H-%M-%S", time.localtime())
             filename = f"./data/{formatted_time}.json"
-            # my_logger.log(filename)  # Log the new filename
-            
+            # my_logger.log(7,filename)  # Log the new filename
+
             while True:
                 try:
                     data = await asyncio.wait_for(websocket.recv(), timeout=5.0)
-                    json_data = json.loads(data)  # Directly parse the received data
-                    
-                    if not ("game" in json_data and "countdown" in json_data["game"] and "progress" in json_data["game"]):
-                        my_logger.log(1,"Required fields are missing in the JSON data")
-                        continue
+                    # Directly parse the received data
+                    json_data = json.loads(data)
 
-                    if json_data.get("game", {}).get("countdown") == 0 or json_data.get("game", {}).get("progress") != 5:  # Check if the game is going on
-                        my_logger.log(2,f'gameProgress: {json_data["game"]["progress"]}\ncountdown: {json_data["game"]["countdown"]}\nNot in game\n')
+                    if not ("game" in json_data and "countdown" in json_data["game"] and "progress" in json_data["game"]):
+                        my_logger.log(
+                            1, f"{formatted_time}\nRequired fields are missing in the JSON data\nJson data: {json_data}\n")
                         break
-                    
+
+                    # Check if the game is going on
+                    if json_data.get("game", {}).get("countdown") == 0 or json_data.get("game", {}).get("progress") != 5:
+                        my_logger.log(
+                            2, f'{formatted_time}\ngameProgress: {json_data["game"]["progress"]}\ncountdown: {json_data["game"]["countdown"]}\nNot in game\n')
+                        break
+
                     # Write data to file
                     with open(filename, 'a') as file:
                         json.dump(json_data, file, indent=4)
                         file.write(',\n')
-                        my_logger.log("writing\n")
-                        my_logger.log(3,f'countdown: {json_data["game"]["countdown"]}\n')
-                
+                        my_logger.log(
+                            3, f'{formatted_time}\nwriting\ncountdown: {json_data["game"]["countdown"]}\n')
+
                 except asyncio.TimeoutError:
-                    my_logger.log(4,"No data received in the last 5 seconds")
+                    my_logger.log(
+                        4, f"{formatted_time}\nNo data received in the last 5 seconds\n")
                 except (json.JSONDecodeError, KeyError) as e:
-                    my_logger.log(5,f"Data parsing error: {e}\nJson data: {json_data}")
+                    my_logger.log(
+                        5, f"{formatted_time}\nData parsing error: {e}\nJson data: {json_data}\n")
                 except Exception as e:
-                    my_logger.log(6,f"An error occurred: {e}")
+                    my_logger.log(
+                        6, f"{formatted_time}\nAn error occurred: {e}\n")
 
 url = "wss://presentation.robomaster.com/api/ws/subscriber"
 asyncio.get_event_loop().run_until_complete(get_wss_data(url))
